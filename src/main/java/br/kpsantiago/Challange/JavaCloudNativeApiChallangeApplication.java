76cedfc4af042aaeaf36abc0b9d54c63c681d3c5
KpSantiago <santiago.kauajose11@gmail.com>
2025-04-18 16:39:46 -0300

    initial commit

---
+++ src/main/java/br/kpsantiago/Challange/JavaCloudNativeApiChallangeApplication.java
@@ -0,0 +1,13 @@
+package br.kpsantiago.Challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCloudNativeApiChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCloudNativeApiChallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/kpsantiago/Challange/JavaCloudNativeApiChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.kpsantiago.Challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCloudNativeApiChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
